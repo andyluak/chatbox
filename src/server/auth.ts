dbfbc38730027cfd93737db9fe54c150c8bb53cc
Alex Tirim <49715292+andyluak@users.noreply.github.com>
2023-04-28 21:07:12 +0300

    .

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,11 +1,12 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { type GetServerSidePropsContext } from "next";
 import {
-  getServerSession,
-  type NextAuthOptions,
   type DefaultSession,
+  type NextAuthOptions,
+  getServerSession,
 } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
 import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
 
