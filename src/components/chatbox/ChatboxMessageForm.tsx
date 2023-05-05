8ad3bd005aed443252bb6aaba0b11e477ec41431
Alex Tirim <49715292+andyluak@users.noreply.github.com>
2023-05-05 20:43:51 +0300

    .

--- src/components/chatbox/ChatboxMessageForm.tsx
+++ src/components/chatbox/ChatboxMessageForm.tsx
@@ -1,25 +1,67 @@
 import clsx from "clsx";
+import { useAtom } from "jotai";
 import { Send } from "lucide-react";
 import React from "react";
 
 import { Input } from "../ui/input";
+import { selectedExpertAtom } from "~/store/chatbox";
 
 const ChatboxMessageForm = () => {
+    const [selectedExpert] = useAtom(selectedExpertAtom);
+    const buildExpertInputs = () => {
+        return (
+            <div
+                className={clsx(
+                    "flex w-full max-w-xl flex-row gap-x-10 gap-y-4 ",
+                    {
+                        "flex-wrap": selectedExpert?.variables.length !== 1,
+                    }
+                )}
+            >
+                {selectedExpert?.variables.map((v) => {
+                    return (
+                        <>
+                            <Input
+                                key={v}
+                                className="max-w-xl flex-grow bg-slate-800"
+                                placeholder={v}
+                                name={v}
+                            />
+                        </>
+                    );
+                })}
+            </div>
+        );
+    };
+
     return (
         <form
             className={clsx(
-                "flex flex-row items-center justify-center  p-4",
+                "m-auto flex w-full max-w-xl flex-row items-center justify-center p-4",
                 "[&_svg]:ml-2 [&_svg]:stroke-slate-300 [&_svg]:transition-[stroke] hover:[&_svg]:stroke-slate-400"
             )}
+            
         >
-            <Input
-                className="max-w-xl bg-slate-800"
-                placeholder="Send a message"
-            />
-            <button type="submit">
-                <Send />
-                <span className="sr-only">Send</span>
-            </button>
+            {selectedExpert ? (
+                <>
+                    {buildExpertInputs()}
+                    <button type="submit" className="flex flex-row gap-4">
+                        <Send />
+                        <span>Submit</span>
+                    </button>
+                </>
+            ) : (
+                <>
+                    <Input
+                        className="max-w-xl bg-slate-800"
+                        placeholder="Send a message"
+                    />
+                    <button type="submit">
+                        <Send />
+                        <span className="sr-only">Send</span>
+                    </button>
+                </>
+            )}
         </form>
     );
 };
